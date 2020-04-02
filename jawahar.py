325c0cba8724e347b074166dcc6c7a2692622c31
jawaharbabugit <45632153+jawaharbabugit@users.noreply.github.com>
2020-04-02 23:49:38 +0530

    Update jawahar.py

--- jawahar.py
+++ jawahar.py
@@ -1,2 +1,12 @@
 for i in a:
     print(data[i].value_counts())
+
+    
+z=['Street','Utilities','Condition2','RoofMatl','Heating','HeatingQC']
+datac=pd.DataFrame(data[a],columns=a)
+print(datac.head())
+
+for i in z:
+    datac.drop(i,axis=1)
+for i in a: 
+    data.drop(i,axis=1)
