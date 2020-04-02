112b9657b9677a3aefe76be117a38310b8544d22
jawaharbabugit <45632153+jawaharbabugit@users.noreply.github.com>
2020-04-02 23:38:40 +0530

    Update jawahar.py

--- jawahar.py
+++ jawahar.py
@@ -1,7 +1,2 @@
-
-for i in list(data.columns):
-    if type(data.loc[0,i])==str:
-        print(i)
-
-a = [i for i in list(df2.columns) if type(df2.loc[0,i])==str]
-print(a)   
+for i in a:
+    print(data[i].value_counts())
