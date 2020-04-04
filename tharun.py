0f0e44d67f9a1205358ba4cb4d9b646957b6b65b
TeslaLord <40894505+TeslaLord@users.noreply.github.com>
2020-04-04 23:00:44 +0530

    Update tharun.py

--- tharun.py
+++ tharun.py
@@ -1,4 +1,18 @@
-for i in range(len(data.columns)):
-    cat = data.columns[i]
-    encoder = LabelBinarizer()
-    catout = encoder.fit_transform(data[cat])
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+num_attribs = list(datan)
+cat_attribs = ["ocean_proximity"]
+num_pipeline = Pipeline([
+('selector', DataFrameSelector(num_attribs)),
+('imputer', Imputer(strategy="median")),
+('attribs_adder', CombinedAttributesAdder()),
+('std_scaler', StandardScaler()),
+])
+cat_pipeline = Pipeline([
+('selector', DataFrameSelector(cat_attribs)),
+('label_binarizer', LabelBinarizer()),
+])
+full_pipeline = FeatureUnion(transformer_list=[
+("num_pipeline", num_pipeline),
+("cat_pipeline", cat_pipeline),
+])
