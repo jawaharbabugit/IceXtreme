bd82d955df8f0585af4d379328d278690caaca07
Jawahar Babu S <45632153+jawaharbabugit@users.noreply.github.com>
2020-04-04 23:00:11 +0530

    Update jawahar.py

--- jawahar.py
+++ jawahar.py
@@ -16,3 +16,31 @@ for i in list(masked.index):
     data.drop(i,axis=1,inplace=True)
 biased=['Street','GarageType', 'GarageFinish', 'GarageQual', 'GarageCond','MSZoning','Condition1','RoofStyle']
 datac.drop(biased,axis=1,inplace=True)
+
+
+
+
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
+class MyLabelBinarizer(TransformerMixin):
+    def __init__(self, *args, **kwargs):
+        self.encoder = LabelBinarizer(*args, **kwargs)
+    def fit(self, x, y=0):
+        self.encoder.fit(x)
+        return self
+    def transform(self, x, y=0):
+        return self.encoder.transform(x)
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attribute_names].values
+from sklearn.pipeline import FeatureUnion
+num_attribs = datan.columns
+cat_attribs = datac.columns
+num_pipeline = Pipeline([('selector', DataFrameSelector(num_attribs)),('imputer', SimpleImputer(strategy="median")),('std_scaler', StandardScaler()),])
+cat_pipeline = Pipeline([('selector', DataFrameSelector(cat_attribs)),('label_binarizer', MyLabelBinarizer()),])
+full_pipeline = FeatureUnion(transformer_list=[("num_pipeline", num_pipeline),("cat_pipeline", cat_pipeline),])
