b2cbe81625b949bd2443dcfa13850475d0b5168c
TeslaLord <40894505+TeslaLord@users.noreply.github.com>
2020-04-04 23:16:41 +0530

    Update tharun.py

--- tharun.py
+++ tharun.py
@@ -1,26 +1,10 @@
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import FeatureUnion
-num_attribs = list(datan)
-cat_attribs = list(datac)
-num_pipeline = Pipeline([
-('selector', DataFrameSelector(num_attribs)),
-('imputer', SimpleImputer(strategy="median")),
-('std_scaler', StandardScaler()),
-])
-cat_pipeline = Pipeline([
-('selector', DataFrameSelector(cat_attribs)),
-('label_binarizer', LabelBinarizer()),
-])
-full_pipeline = FeatureUnion(transformer_list=[
-("num_pipeline", num_pipeline),
-("cat_pipeline", cat_pipeline),
-])
 
-from sklearn.base import BaseEstimator, TransformerMixin
-class DataFrameSelector(BaseEstimator, TransformerMixin):
- def __init__(self, attribute_names):
-  self.attribute_names = attribute_names
- def fit(self, X, y=None):
-  return self
- def transform(self, X):
-  return X[self.attribute_names].values
+biased=['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond','MSZoning','Condition1','RoofStyle']
+datac = datac.drop(biased, axis = 1)
+
+
+narrow=['EnclosedPorch','KitchenAbvGr']
+datan = datan.drop(narrow, axis = 1)
+
+z=['Street','Utilities','Condition2','RoofMatl','Heating','HeatingQC']
+data = df2.drop(z,axis =1)
