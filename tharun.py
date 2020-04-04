ef09028130cf1e7b9c65766d6aeab44d1dbff2cc
TeslaLord <40894505+TeslaLord@users.noreply.github.com>
2020-04-04 23:13:30 +0530

    Update tharun.py

--- tharun.py
+++ tharun.py
@@ -1,11 +1,10 @@
+from sklearn.impute import SimpleImputer
 from sklearn.pipeline import FeatureUnion
-from sklearn.pipeline import Pipeline
 num_attribs = list(datan)
-cat_attribs = ["ocean_proximity"]
+cat_attribs = list(datac)
 num_pipeline = Pipeline([
 ('selector', DataFrameSelector(num_attribs)),
-('imputer', Imputer(strategy="median")),
-('attribs_adder', CombinedAttributesAdder()),
+('imputer', SimpleImputer(strategy="median")),
 ('std_scaler', StandardScaler()),
 ])
 cat_pipeline = Pipeline([
@@ -16,3 +15,12 @@ full_pipeline = FeatureUnion(transformer_list=[
 ("num_pipeline", num_pipeline),
 ("cat_pipeline", cat_pipeline),
 ])
+
+from sklearn.base import BaseEstimator, TransformerMixin
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+ def __init__(self, attribute_names):
+  self.attribute_names = attribute_names
+ def fit(self, X, y=None):
+  return self
+ def transform(self, X):
+  return X[self.attribute_names].values
