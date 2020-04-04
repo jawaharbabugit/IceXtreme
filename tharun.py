cff409587365379261d2b85b6a9642fdee865fe4
TeslaLord <40894505+TeslaLord@users.noreply.github.com>
2020-04-04 22:21:18 +0530

    Update tharun.py

--- tharun.py
+++ tharun.py
@@ -1,25 +1,4 @@
-num_attribs = list(datan)
-cat_attribs = list(datac)
-
-from sklearn.base import BaseEstimator, TransformerMixin
-class DataFrameSelector(BaseEstimator, TransformerMixin):
- def __init__(self, attribute_names):
-  self.attribute_names = attribute_names
- def fit(self, X, y=None):
-  return self
- def transform(self, X):
-  return X[self.attribute_names].values
-
-num_pipeline = Pipeline([
-('selector', DataFrameSelector(num_attribs)),
-('imputer', SimpleImputer(strategy="median")),
-('std_scaler', StandardScaler()),
-])
-cat_pipeline = Pipeline([
-('selector', DataFrameSelector(cat_attribs)),
-('label_binarizer', LabelBinarizer()),
-])
-full_pipeline = FeatureUnion(transformer_list=[
-("num_pipeline", num_pipeline),
-("cat_pipeline", cat_pipeline),
-])
+for i in range(len(data.columns)):
+    cat = data.columns[i]
+    encoder = LabelBinarizer()
+    catout = encoder.fit_transform(data[cat])
