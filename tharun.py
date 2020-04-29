88ccee3070f02b65b3fa940373334c44d049d9bc
Jawahar Babu S <45632153+jawaharbabugit@users.noreply.github.com>
2020-04-29 18:40:23 +0530

    Update tharun.py

--- tharun.py
+++ tharun.py
@@ -8,4 +8,28 @@ def wrapper(f):
         return l
     return fun
 
+def wrapper(f):
+    def fun(l):
+    
+        for i in range(len(l)):
+            no=str(l[i])
+            if no.startswith('+91'):
+                no.replace('+91','')
+
+            elif (no.startswith('91') and len(no)>10):
+
+                no.replace('91','')
+
+            elif no.startswith('0'):
+                no.replace('0','')
+
+            first='+91'
+            sec=no[:5]
+            third=no[5:]
+            no=first+' '+sec+' '+third
+            a[i]=no
+        
+
+
+    return f
 
