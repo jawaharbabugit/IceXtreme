a7e8fadccff653b98eb8e26abb1fa1772a79c5e9
Jawahar Babu S <45632153+jawaharbabugit@users.noreply.github.com>
2020-05-15 19:11:35 +0530

    Update jawahar.py

--- jawahar.py
+++ jawahar.py
@@ -1,46 +1,43 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-data=pd.read_csv('../input/house-prices-advanced-regression-techniques/train.csv')
-data.dropna(axis=1,thresh=1000,inplace=True)
-narrow=['PoolArea','3SsnPorch','EnclosedPorch','BsmtFinSF2','BsmtHalfBath','KitchenAbvGr','LowQualFinSF','MiscVal','ScreenPorch']
-for i in narrow:
-    data.drop(i,inplace=True,axis=1)
-dtype=data.dtypes
-mask=(dtype=='object')
-datac=pd.DataFrame(columns=None)
-masked=dtype[mask]
-for i in list(masked.index):
-    datac[i]=data[i]
-    data.drop(i,axis=1,inplace=True)
-biased=['Street','GarageType', 'GarageFinish', 'GarageQual', 'GarageCond','MSZoning','Condition1','RoofStyle']
-datac.drop(biased,axis=1,inplace=True)
+#!/usr/bin/python
 
+# Head ends here
 
-
-
-
-
-from sklearn.base import BaseEstimator, TransformerMixin
-class MyLabelBinarizer(TransformerMixin):
-    def __init__(self, *args, **kwargs):
-        self.encoder = LabelBinarizer(*args, **kwargs)
-    def fit(self, x, y=0):
-        self.encoder.fit(x)
-        return self
-    def transform(self, x, y=0):
-        return self.encoder.transform(x)
-class DataFrameSelector(BaseEstimator, TransformerMixin):
-    def __init__(self, attribute_names):
-        self.attribute_names = attribute_names
-    def fit(self, X, y=None):
-        return self
-    def transform(self, X):
-        return X[self.attribute_names].values
-from sklearn.pipeline import FeatureUnion
-num_attribs = datan.columns
-cat_attribs = datac.columns
-num_pipeline = Pipeline([('selector', DataFrameSelector(num_attribs)),('imputer', SimpleImputer(strategy="median")),('std_scaler', StandardScaler()),])
-cat_pipeline = Pipeline([('selector', DataFrameSelector(cat_attribs)),('label_binarizer', MyLabelBinarizer()),])
-full_pipeline = FeatureUnion(transformer_list=[("num_pipeline", num_pipeline),("cat_pipeline", cat_pipeline),])
+import math
+def next_move(pos,board):
+    p=[]
+    c=0
+    a=0
+   
+    for l in board:
+        count=0
+        for i in l:
+            if i=='d':
+                p.append([c,count])
+            count+=1
+        c+=1
+    short=10
+    for i in p:
+        score=math.sqrt(((pos[0]-i[0])**2)+((pos[1]-i[1])**2))
+        if score<short:
+            short=score
+            p1=i
+    if p1==pos:
+        print('CLEAN')
+    elif p1[1]!=pos[1]:
+        if pos[1]>p1[1]:
+            print('LEFT')
+            pos[1]-=1
+        elif pos[1]<p1[1]:
+            print('RIGHT')
+            pos[1]+=1 
+    elif p1[0]!=pos[0]:
+        if pos[0]>p1[0]:
+            print('UP')
+            pos[0]-=1
+        elif pos[0]<p1[0]:
+            print('DOWN')
+            pos[0]+=1
+if __name__ == "__main__":
+    pos = [int(i) for i in input().strip().split()]
+    board = [[j for j in input().strip()] for i in range(5)]
+    next_move(pos, board)
